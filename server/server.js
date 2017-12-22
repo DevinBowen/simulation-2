a29004b509036a30481bb70370343a9f466bd293
devin <deivnobowen@gmail.com>
2017-12-22 14:55:19 -0700

    i think the server is done getting set up

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const bodyParser = require('body-parser')
     ,express = require('express')
     ,session = require('express-session')
+    ,controller = require('./controller')
+    ,cors = require('cors')
+    ,massive = require('massive');
 require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
+app.use( cors() );
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
@@ -15,4 +19,7 @@ app.use(session({
 
 
 const port = process.env.PORT
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db', db)
 app.listen(port, console.log(`this server is running on port ${port}.`))
+})
